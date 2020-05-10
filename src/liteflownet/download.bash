5bc7dbf7901350060e377bf321fe9caed08bc503
zhang-cx <zhangcx@berkeley.edu>
2020-05-10 21:50:36 +0000

    update

---
+++ src/liteflownet/download.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-default.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-kitti.pytorch
+wget --verbose --continue --timestamping http://content.sniklaus.com/github/pytorch-liteflownet/network-sintel.pytorch
